02e372b499849498ce992d977e34f0940ec32e9a
jgtaylor <sonicbphuct@gmail.com>
2017-11-17 17:58:37 +0100

    inital commit

---
+++ src/Input.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="">
+		<input type="text" v-model="message" @input="changeMessage">
+		<p>{{ message }}</p>
+	</div>
+</template>
+<script>
+	export default {
+		props: [ "msg"],
+		data() {
+			return {
+				message: ""
+			}
+		},
+		methods: {
+			changeMessage(event) {
+				this.message = event.target.value
+				this.$emit("changeMessage", this.message)
+			}
+		}
+	}
+</script>
---
+++ src/Message.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h1>{{ message }}</h1>
+		<app-input :msg="message" @changeMessage="message = $event"></app-input>
+	</div>
+</template>
+<script>
+	import Input from "./Input.vue"
+
+	export default {
+		data() {
+			return {
+				message: "This is a great message"
+			}
+		},
+		components: {
+			"app-input": Input
+		}
+	}
+</script>
