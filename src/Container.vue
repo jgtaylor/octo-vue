10f76f451dcd1fd5b0da155b08958748a835c5e5
jgtaylor <sonicbphuct@gmail.com>
2017-11-27 15:09:18 +0100

    not sure what i did
    add components, i think.

--- src/Container.vue
+++ src/Container.vue
@@ -1,12 +1,26 @@
 <template>
 	<div class="">
-
+		<app-devices></app-devices>
+		<app-zones></app-zones>
+		<app-schedules></app-schedules>
 	</div>
 </template>
+
 <script>
+	import Devices from './Devices.vue'
+	import Zones from './Zones.vue'
+	import Schedules from './Schedules.vue'
+
 	export default {
 		data() {
-			return {}
+			return {
+
+			}
+		}
+		components: {
+			'app-devices': Devices,
+			'app-zones': Zones,
+			'app-schedules': Schedules
 		}
 	}
 </script>
