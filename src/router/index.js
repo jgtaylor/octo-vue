48a904776cb1cbbca3c125d33226bea5b4b7a7b1
jgtaylor <sonicbphuct@gmail.com>
2017-12-01 18:36:22 +0100

    basic navagation is functional

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import DeviceDetails from '@/components/DeviceDetails'
+import Zones from '@/components/Zones'
+import Devices from '@/components/Devices'
+import Schedules from '@/components/Schedules'
+import PidControlers from '@/components/PidControlers'
 
 Vue.use( Router )
 let ws = new WebSocket( "ws://localhost:2801/" );
@@ -11,18 +14,54 @@ ws.addEventListener( 'close', function ( event ) {
 
 export default new Router( {
 	routes: [ {
-		path: '/',
-		name: 'Home',
+		path: '',
+		name: 'homeDefault',
 		component: Home,
 		meta: {
 			ws: ws
 		}
 	}, {
-		path: '/device/:device',
-		name: 'DeviceDetails',
-		component: DeviceDetails,
+		path: '/home',
+		name: 'homeLong',
+		component: Home,
 		meta: {
 			ws: ws
 		}
-	} ]
+	}, {
+		path: '/',
+		name: 'homeShort',
+		component: Home,
+		meta: {
+			ws: ws
+		}
+	}, {
+		path: '/zones',
+		name: 'zones',
+		component: Zones,
+		meta: {
+			ws: ws
+		}
+	}, {
+		path: '/devices',
+		name: 'devices',
+		component: Devices,
+		meta: {
+			ws: ws
+		}
+	}, {
+		path: '/schedules',
+		name: 'schedules',
+		component: Schedules,
+		meta: {
+			ws: ws
+		}
+	}, {
+		path: '/pidcontrolers',
+		name: 'pidcontrolers',
+		component: PidControlers,
+		meta: {
+			ws: ws
+		}
+	}, ],
+	mode: "history"
 } )
