e7b44900b9da94201d3fadeed8ef4b58d8f04543
jgtaylor <sonicbphuct@gmail.com>
2017-12-02 14:32:53 +0100

    fix routing, add containers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
 <div id="app">
-	<el-tabs type="border-card" @tab-click="handleClick">
-		<el-tab-pane label="Home">Home</el-tab-pane>
-		<el-tab-pane label="Zones">Zones</el-tab-pane>
-	<el-tab-pane label="Devices">Devices</el-tab-pane>
-	<el-tab-pane label="Schedules">Schedules</el-tab-pane>
-	<el-tab-pane label="PIDControlers">PID Controlers</el-tab-pane>
-	</el-tabs>
-	<router-view/>
+	<el-container>
+		<el-header>
+			<el-tabs type="border-card" @tab-click="handleClick">
+				<el-tab-pane label="Home" name="home">Home</el-tab-pane>
+				<el-tab-pane label="Zones" name="zones">Zones</el-tab-pane>
+				<el-tab-pane label="Devices" name="devices">Devices</el-tab-pane>
+				<el-tab-pane label="Schedules" name="schedules">Schedules</el-tab-pane>
+				<el-tab-pane label="PIDControlers" name="pidcontrolers">PID Controlers</el-tab-pane>
+			</el-tabs>
+		</el-header>
+		<el-main>
+			<router-view/>
+		</el-main>
+	</el-container>
 </div>
 </template>
 
@@ -15,23 +21,25 @@
 export default {
 	name: 'app',
 	data() {
+		console.log("Inside App.vue:data()")
 		return {
 			activeName: 'first'
 		}
 	},
 	methods: {
-    	handleClick(tab, event) {
+		handleClick(tab, event) {
+			console.log("inside App.vue:handleClick()")
 			switch (tab.label) {
-				case "home":
-					tab.label = "";
+				case "Home":
+					this.$router.push("/")
 					break;
 				default:
+					this.$router.push("/" + tab.label.toLowerCase());
 					break;
 			}
-			this.$router.push("/"+tab.label.toLowerCase())
-			console.log(event);
-    	}
-    }
+
+		}
+	}
 }
 </script>
 
@@ -42,6 +50,7 @@ export default {
 	-moz-osx-font-smoothing: grayscale;
 	color: #2c3e50;
 	margin-top: 30px;
+	width: 640px;
 }
 
 .link {
