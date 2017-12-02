b63f6aa1061f6182617ab060c22c65873ec024a6
jgtaylor <sonicbphuct@gmail.com>
2017-12-02 14:34:05 +0100

    text & formating

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="home">
 	<h2>OctoBox Management</h2>
-	<img src="https://upload.wikimedia.org/wikipedia/commons/9/97/Twemoji2_1f419.svg" width="150px" class="copy-img">
+	<img src="https://upload.wikimedia.org/wikipedia/commons/9/97/Twemoji2_1f419.svg" width="75px" class="copy-img">
 	<p class="copy">Welcome to the OctoBox Project. Here we'll configure
 		everything your box needs to manage your garden, your pool, your aquarium,
 	or anything else you need to manage.</p>
@@ -11,8 +11,14 @@
 		you might setup an Herbal Zone &amp; a Vegitable Zone.</li>
 	<li>Associate Devices with Zones: Your devices should appear here auto:magically.
 	Once they're available, you tell OctoBox in which Zone your device is operating.
-	This can potentially get confusing, so make sure you've got the right one in the right place.</li>
-	<li>Finally, you associate a Schedule or a PID Controller, or both, for your Zones.</li>
+	This can potentially get confusing, so make sure you've got the right one in
+	the right place.</li>
+	<li>Finally, you associate a Schedule or a PID Controller, or both, for your
+		Zones, or even for an individual device.</li>
+
+	Eventually, you'll be able to monitor your automated things, add new things
+	and capabilities easily, integrating them into your already running systems.
+	The possiblilities are truely endless!
 </ol>
 That's it! As easy as 1 - 2 - 3!
 	</p>
@@ -47,6 +53,7 @@ p {
 }
 .copy-img {
 	float: left;
-	margin: 30px;
+	margin: 0px 30px 30px 30px;
+
 }
 </style>
