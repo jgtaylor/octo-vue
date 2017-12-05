024754d687c8b65f824e3564551dbe56da80c084
jgtaylor <sonicbphuct@gmail.com>
2017-12-05 01:05:06 +0100

    adde object counts, using ws.eventing.

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -2,6 +2,20 @@
 <div class="aside">
 	<div v-html="connMsg"></div>
 	<p><i class="el-icon-info"></i> Here we could have realtime messages, etc. This is Aside.vue.</p>
+	<table>
+		<tr>
+			<td class="table-title">Zones</td><td class="table-number" >{{zones}}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Devices</td><td class="table-number" >{{devices}}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Schedules</td><td class="table-number" >{{schedules}}</td>
+		</tr>
+		<tr>
+			<td class="table-title">Pid Controllers</td><td class="table-number" >{{pidCtlrs}}</td>
+		</tr>
+	</table>
 </div>
 </template>
 
@@ -11,7 +25,12 @@ export default {
 		return {
 			ws: this.$route.meta.ws,
 			connected: this.$route.meta.ws.readyState,
-			connMsg: ''
+			connMsg: '',
+			lm: this.$route.meta.ws.lm,
+			zones: 0,
+			devices: 0,
+			schedules: 0,
+			pidCtlrs: 0
 		}
 	},
 	created() {
@@ -25,11 +44,30 @@ export default {
 		vm.ws.addEventListener("error", function() {
 			vm.connMsg = `<i class="el-icon-error"> </i> WebSocket has an error.`
 		})
+		vm.ws.addEventListener("lm-updated", function(e) {
+			console.log(e.detail);
+			vm.zones = e.detail.zones.length;
+			vm.devices = e.detail.devices.length;
+			vm.schedules = e.detail.schedules.length;
+			vm.pidCtlrs = e.detail.pidCtlrs.length;
+		})
 	}
 }
 </script>
 
 <style lang="css">
+.table-number {
+	text-align: right;
+	width: 30px;
+	font-size: .8em;
+	font-weight: bold;
+}
+
+.table-title {
+	text-align: left;
+	font-size: .8em;
+	font-weight: bold;
+}
 
 .WS-error {
 	color: red;
