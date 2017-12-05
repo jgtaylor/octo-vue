91274ec1ad8edcba372f40dfab38276f4bda42d3
jgtaylor <sonicbphuct@gmail.com>
2017-12-05 01:03:41 +0100

    added the guts

--- src/components/Zones.vue
+++ src/components/Zones.vue
@@ -1,13 +1,34 @@
 <template lang="html">
+	<div class="container">
+			<ul>
+				<li v-for="zone in zones"><router-link :to="{ name: 'zoneDetails', params: {zoneID: zone} }">{{ zone }}</router-link></li>
+			</ul>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			ws: this.$route.ws,
-			zones: {}
+			ws: this.$route.meta.ws,
+			zones: []
 		}
+	},
+	created() {
+		let vm = this;
+		// TODO: all this name shit should really be zoneID.
+		if ( vm.ws.readyState === 1 ) {
+			vm.zones = vm.ws.lm.zones.map(z => z.name );
+		}
+		vm.ws.addEventListener("lm-updated", function(e) {
+			vm.zones = e.detail.zones.map(z => z.name);
+			console.log("Devices.vue: created() - event 'lm-updated'");
+		});
+
+		vm.ws.addEventListener("zone-add", function(e) {
+			vm.zones.push(e.detail.name);
+			console.log("added zone %s", e.detail.name);
+		});
 	}
 }
 </script>
